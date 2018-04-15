96e4842b26374187d8be8df579a02cee84b7d211
Nikolay Shulga <nshulga@gmail.com>
2018-04-15 18:20:56 -0400

    Started conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,35 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.time.Year
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        // compareTo must return 0 for same dates, negative or positive number otherwise.
+        // There is no requirement to return a consistent "distance" between values, so
+        // let's keep things simple.
+        if (year != other.year)  return year - other.year
+        if (month != other.month) return month - other.month
+        if (dayOfMonth != other.dayOfMonth) return dayOfMonth - other.dayOfMonth
+        return  0
+
+        // Interestingly enough, a Calendar base implementation doesn't compute exact 'distance' in days either.
+        // Performance, I guess, or maybe Calendar just wasn't implemented properly. <insert a comment about Java and toaster ovens>.
+        /*
+        val c= Calendar.getInstance()
+        c.set(year, month, dayOfMonth)
+        val c_other = Calendar.getInstance()
+        c_other.set(other.year, other.month, other.dayOfMonth)
+        val cmp = c.compareTo(c_other)
+        return cmp
+        */
+
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +37,11 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateIterator(val range: DateRange, var current:MyDate = range.start): Iterator<MyDate> {
+override fun hasNext(): Boolean { return current <= range.endInclusive}
+override fun next(): MyDate { val  retval = current; current = current.nextDay(); return retval} // is there an equivalent of postfix?
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> { return DateIterator(this) }
+}
